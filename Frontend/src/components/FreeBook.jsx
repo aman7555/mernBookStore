d2488f42febe5528fb598e6928da1a5b573aa0b4
Aman Rai <aman@gmail.com>
2024-06-11 13:34:54 +0530

    Hello

--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -11,9 +11,7 @@ const FreeBook = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get(
-          "https://book-store-cqgg.onrender.com/book"
-        );
+        const res = await axios.get("http://localhost:4001/book");
         const data = res.data.filter((data) => data.category === "Free");
         setBook(data);
         console.log(data);
